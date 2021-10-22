07db599b0e84d4e745efcff5dcdc6959fb1a4fa7
HamdanFauzi211 <hamdanfauzi281@gmail.com>
2021-10-22 15:01:14 +0700

    Layout Selesai

---
+++ src/mini_projek/app/src/main/java/com/kuliah/miniproject/halaman_awal.java
@@ -0,0 +1,12 @@
+package org.aplas.mini_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class halaman_awal extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.halaman_awal);
+    }
+}
