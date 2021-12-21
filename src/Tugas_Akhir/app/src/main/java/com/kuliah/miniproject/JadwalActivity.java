00661b9277b7feb2b7335af527a5b095271f781a
khoirulwhyd <gamingkenzo7@gmail.com>
2021-12-21 11:42:53 +0700

    commit baru tugas akhir

---
+++ src/Tugas_Akhir/app/src/main/java/com/kuliah/miniproject/JadwalActivity.java
@@ -0,0 +1,16 @@
+package com.kuliah.miniproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JadwalActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.jadwal);
+        }
+}
--- src/mini_projek/app/src/main/java/com/kuliah/miniproject/JadwalActivity.java
+++ src/mini_projek/app/src/main/java/com/kuliah/miniproject/JadwalActivity.java
@@ -12,13 +12,5 @@ public class JadwalActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.jadwal);
-
-            ImageButton btnPesan1 = findViewById(R.id.pesanJadwal_1);
-            ImageButton btnPesan2 = findViewById(R.id.pesanJadwal_2);
-        }
-
-        public void pesanJadwal (View view){
-            Intent i = new Intent(JadwalActivity.this, pesanActivity.class);
-            startActivity(i);
         }
 }
