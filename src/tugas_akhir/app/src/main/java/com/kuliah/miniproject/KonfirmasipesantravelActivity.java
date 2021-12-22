50df74d862b7de182207c5f03ba033193fa3aebc
khoirulwhyd <gamingkenzo7@gmail.com>
2021-12-22 08:00:18 +0700

    layout clear

---
+++ src/tugas_akhir/app/src/main/java/com/kuliah/miniproject/KonfirmasipesantravelActivity.java
@@ -0,0 +1,14 @@
+package com.kuliah.miniproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KonfirmasipesantravelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_konfirmasipesantravel);
+    }
+}
