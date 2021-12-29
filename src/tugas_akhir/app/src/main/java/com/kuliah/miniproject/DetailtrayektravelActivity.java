75e26584474412cc48a794e614ccd8b742191306
HamdanFauzi211 <hamdanfauzi281@gmail.com>
2021-12-29 09:27:10 +0700

    Menambah intent halaman Trayek ke Rincian

--- src/tugas_akhir/app/src/main/java/com/kuliah/miniproject/DetailtrayektravelActivity.java
+++ src/tugas_akhir/app/src/main/java/com/kuliah/miniproject/DetailtrayektravelActivity.java
@@ -2,13 +2,20 @@ package com.kuliah.miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DetailtrayektravelActivity extends AppCompatActivity {
-
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailtrayektravel);
+    }
+    public void pindah5(View view) {
+        Intent intent = new Intent(DetailtrayektravelActivity.this, RinciantrayekActivity.class);
+        startActivity(intent);
     }
 }
