0136da1ba575c7ea6abd6284bc0ff857938cf86f
HamdanFauzi211 <hamdanfauzi281@gmail.com>
2021-12-29 09:36:08 +0700

    Menambah intent rincian ke konfirmasi pesan

--- src/tugas_akhir/app/src/main/java/com/kuliah/miniproject/RinciantrayekActivity.java
+++ src/tugas_akhir/app/src/main/java/com/kuliah/miniproject/RinciantrayekActivity.java
@@ -2,13 +2,21 @@ package com.kuliah.miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class RinciantrayekActivity extends AppCompatActivity {
-
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rinciantrayek);
+    }
+
+    public void pindah6(View view) {
+        Intent intent = new Intent(RinciantrayekActivity.this, KonfirmasipesantravelActivity.class);
+        startActivity(intent);
     }
 }
