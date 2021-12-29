005f349404a4a1c6b5f8c86c8a092d1dfd24ba64
HamdanFauzi211 <hamdanfauzi281@gmail.com>
2021-12-29 13:03:50 +0700

    Menambah intent halaman pembayaran ke invoice

--- src/tugas_akhir/app/src/main/java/com/kuliah/miniproject/CheckoutmetodepembayaranActivity.java
+++ src/tugas_akhir/app/src/main/java/com/kuliah/miniproject/CheckoutmetodepembayaranActivity.java
@@ -2,13 +2,21 @@ package com.kuliah.miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CheckoutmetodepembayaranActivity extends AppCompatActivity {
-
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkoutmetodepembayaran);
+    }
+
+    public void pindah9(View view) {
+        Intent intent = new Intent(CheckoutmetodepembayaranActivity.this, InvoiceActivity.class);
+        startActivity(intent);
     }
 }
