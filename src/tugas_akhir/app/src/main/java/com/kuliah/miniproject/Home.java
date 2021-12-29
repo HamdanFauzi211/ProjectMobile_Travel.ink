0afb1316b510ea10447bbb5855c5212c72d96781
HamdanFauzi211 <hamdanfauzi281@gmail.com>
2021-12-29 09:04:55 +0700

    Menambah Intent untuk halaman Home ke Jadwal

--- src/tugas_akhir/app/src/main/java/com/kuliah/miniproject/Home.java
+++ src/tugas_akhir/app/src/main/java/com/kuliah/miniproject/Home.java
@@ -1,14 +1,24 @@
-package com.kuliah.miniproject;
+ package com.kuliah.miniproject;
 
+import androidx.annotation.DimenRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void pindah1(View view) {
+        Intent intent = new Intent(Home.this, JadwalActivity.class);
+        startActivity(intent);
     }
 }
